3b9233f2a7f5680071ce0fbbb9a99186cd2d6872
Muhammad Tayyab <codewithtayyab1@gmail.com>
2026-07-12 16:08:35 +0500

    Fix hardcoded API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
 })
 
-// Attach admin JWT to every request that has a token in storage
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('bhe_admin_token')
   if (token) {
