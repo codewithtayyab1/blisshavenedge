084dc3962968eef48e725627b3fdf9c08b46da39
Muhammad Tayyab <www.ta09011@gmail.com>
2026-07-23 17:16:09 +0500

    Regenerate frontend lockfile

--- frontend/dist/assets/use-reduced-motion-BfbjFp4L.js
+++
@@ -1,1 +0,0 @@
-import{x as t,y as o,q as r,z as s}from"./index-fhQ0I8jV.js";function u(){!t.current&&o();const[e]=r.useState(s.current);return e}export{u};
---
+++ frontend/dist/assets/use-reduced-motion-CdQ_Ob_J.js
@@ -0,0 +1,1 @@
+import{x as t,y as o,q as r,z as s}from"./index-sIXCdsqs.js";function u(){!t.current&&o();const[e]=r.useState(s.current);return e}export{u};
